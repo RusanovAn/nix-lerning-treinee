1f8724b965ed9c97c667eb90d1ee298a8f209a02
Andrey Rusanov <dostupnodoma@gmail.com>
2020-11-02 00:34:36 +0200

    second leason by beginer

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-Hello, NIX Education
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Nix-learning</title>
+    <meta name="description" content="first site Nix-learning">
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+<header>NIX Education</header>
+<nav class="nav">
+    <a class="nav-link" href="#">Урок 1</a>
+    <a class="nav-link" href="#">Урок 2</a>
+</nav>
+<main>
+    <h1>Hello, NIX Education</h1>
+</main>
+<footer>Футтер тестовый</footer>
+
+</body>
+</html>
+

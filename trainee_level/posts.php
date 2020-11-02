1e79b79188a9c6a8cc5fd938d218a8b9f5c7208a
Andrey Rusanov <dostupnodoma@gmail.com>
2020-11-02 21:40:05 +0200

    add posts list page

--- trainee_level/posts.php
+++ trainee_level/posts.php
@@ -4,9 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <title>Blog Template · Bootstrap</title>
+    <title>NIX-Trainee Level</title>
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="http://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <!-- Custom styles for this template -->
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
     <!-- Custom styles for this template -->
@@ -16,82 +16,79 @@
 <div class="container">
     <header class="blog-header py-3">
         <div class="row flex-nowrap justify-content-between align-items-center">
-            <div class="col-6 text-center">
-                <a class="blog-header-logo text-dark" href="#">Large</a>
+            <div class="col-4 text-center">
+                <a class="blog-header-logo text-dark" href="/">NIX-Trainee Level</a>
             </div>
-            <div class="col-6 d-flex justify-content-end align-items-center">
-                <a class="btn btn-sm btn-outline-secondary" href="registration.php">Регистрация</a>
-                <a class="btn btn-sm btn-outline-secondary" href="login.php">Войти</a>
+            <div class="col-4 d-flex justify-content-end align-items-center">
+                <a class="btn btn-sm btn-outline-primary" href="login.php">Авторизация</a>
+                <span>&nbsp;</span>
+                <a class="btn btn-sm btn-outline-primary" href="registration.php">Регистрация</a>
             </div>
         </div>
     </header>
 
     <div class="nav-scroller py-1 mb-2">
-        <nav class="nav d-flex justify-content-between">
-            <a class="p-2 text-muted" href="#">World</a>
-            <a class="p-2 text-muted" href="#">U.S.</a>
-            <a class="p-2 text-muted" href="#">Technology</a>
-            <a class="p-2 text-muted" href="#">Design</a>
-            <a class="p-2 text-muted" href="#">Culture</a>
-            <a class="p-2 text-muted" href="#">Business</a>
-            <a class="p-2 text-muted" href="#">Politics</a>
-            <a class="p-2 text-muted" href="#">Opinion</a>
-            <a class="p-2 text-muted" href="#">Science</a>
-            <a class="p-2 text-muted" href="#">Health</a>
-            <a class="p-2 text-muted" href="#">Style</a>
-            <a class="p-2 text-muted" href="#">Travel</a>
+        <nav class="nav d-flex justify-content-start">
+            <a class="p-2 text-muted" href="#">Посты</a>
+            <a class="p-2 text-muted" href="#">| Cсылка Два |</a>
+            <a class="p-2 text-muted" href="#">Cсылка три</a>
         </nav>
     </div>
-
 </div>
 <main role="main" class="container">
+    <hr>
     <div class="row">
-        <div class="col-md-12 blog-main">
+        <aside class="col-md-4 blog-sidebar">
+            <div class="p-4 mb-3 bg-light rounded">
+                <h4 class="font-italic">Цитата дня:</h4>
+                <p class="mb-0">Изучай программирование. Знания за плечами не носить</p>
+            </div>
+
+            <div class="p-4">
+                <h4 class="font-italic">Рубрики статей</h4>
+                <ol class="list-unstyled mb-0">
+                    <li><a href="#">PHP</a></li>
+                    <li><a href="#">MySQL</a></li>
+                    <li><a href="#">Java Script</a></li>
+                    <li><a href="#">Ajax</a></li>
+                    <li><a href="#">JQuery</a></li>
+                </ol>
+            </div>
+
+        </aside><!-- /.blog-sidebar -->
+        <div class="col-md-8 blog-main">
             <h3 class="pb-4 mb-4 font-italic border-bottom">
                 Список постов
             </h3>
-
             <div class="blog-post">
-                <h2 class="blog-post-title">First blog post</h2>
-                <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
+                <h4><a href="#" class="text-dark">Первая статья</a></h4>
+                <p class="blog-post-meta">10 Января, 2014 >> Автор: <a href="#">Андрей</a></p>
 
                 <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
-                <blockquote>
-                    <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                </blockquote>
-                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-                <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                <a href="#">Читать статью</a>
+                <hr>
+            </div><!-- /.blog-post -->
+            <div class="blog-post">
+                <h4><a href="#" class="text-dark">Вторая статья</a></h4>
+                <p class="blog-post-meta">10 Января, 2014 >> Автор: <a href="#">Андрей</a></p>
+
+                <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
+                <a href="#">Читать статью</a>
+                <hr>
             </div><!-- /.blog-post -->
 
             <div class="blog-post">
-                <h2 class="blog-post-title">Another blog post</h2>
-                <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
+                <h4><a href="#" class="text-dark">Третяя статья</a></h4>
+                <p class="blog-post-meta">10 Января, 2014 >> Автор: <a href="#">Андрей</a></p>
 
                 <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
-                <blockquote>
-                    <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                </blockquote>
-                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-                <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-            </div><!-- /.blog-post -->
-
-            <div class="blog-post">
-                <h2 class="blog-post-title">New feature</h2>
-                <p class="blog-post-meta">December 14, 2013 by <a href="#">Chris</a></p>
-
-                <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                <ul>
-                    <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
-                    <li>Donec id elit non mi porta gravida at eget metus.</li>
-                    <li>Nulla vitae elit libero, a pharetra augue.</li>
-                </ul>
-                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-                <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
+                <a href="#">Читать статью</a>
+                <hr>
             </div><!-- /.blog-post -->
 
             <nav class="blog-pagination">
-                <a class="btn btn-outline-primary" href="#">Older</a>
-                <a class="btn btn-outline-secondary disabled" href="#" tabindex="-1" aria-disabled="true">Newer</a>
+                <a class="btn btn-outline-secondary disabled" href="#" tabindex="-1" aria-disabled="true">Предыдущая страница</a>
+                <a class="btn btn-outline-primary" href="#">Еще статьи</a>
             </nav>
 
         </div><!-- /.blog-main -->
@@ -99,9 +96,10 @@
 
 </main><!-- /.container -->
 <footer class="blog-footer">
-    <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+    <p>Шаблон блога взят с сайта <a href="https://getbootstrap.com/">Bootstrap</a> Автор шаблона <a href="https://twitter.com/mdo">@mdo</a>.</p>
+    <p>Автор блога <a href="https://www.facebook.com/floverspresent">Русанов Андрей</a></p>
     <p>
-        <a href="#">Back to top</a>
+        <a href="#">На Верх</a>
     </p>
 </footer>
 </body>
